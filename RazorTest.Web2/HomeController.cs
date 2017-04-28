2c84b15e08ccde7c112a3b5e2dda4ca2b1090c3c
Lam Kimserey <kimserey.lam@afusion.com>
2017-04-28 09:19:54 +0800

    more on tag helper

--- RazorTest.Web2/HomeController.cs
+++ RazorTest.Web2/HomeController.cs
@@ -7,23 +7,42 @@ using System.Threading.Tasks;
 
 namespace RazorTest.Web2
 {
-    public class Test
+    public class Company
     {
         [Required]
-        public string Hello { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
     }
 
+    [Route("[controller]")]
     public class HomeController: Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpGet("test")]
-        public IActionResult Test()
+        [HttpGet("companies")]
+        public IActionResult GetCompanies()
         {
-            return View(new Test());
+            return View("Companies", new List<Company> {
+                new Company {
+                    Name = "Helloworld",
+                    Address = "29 street"
+                },
+                new Company {
+                    Name = "Byebyeworld",
+                    Address = "22 street"
+                }
+            });
+        }
+
+        [HttpPost]
+        public IActionResult PostCompany(Company comp)
+        {
+            return Ok();
         }
     }
 }
